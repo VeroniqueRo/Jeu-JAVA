4ecd58762ed86d0f12fb0e36303531c01deaa9f6
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-03-20 17:10:00 +0100

    Ajout des conditions pour les Integer

--- src/main/java/personnage/Jeu.java
+++ src/main/java/personnage/Jeu.java
@@ -75,19 +75,43 @@ public class Jeu {
                     }
 
                 //************************ Donner un niveau de vie au personnage ****************************************
-                System.out.println("Quelle niveau de vie souhaitez-vous donner à votre guerrier ?");
+                System.out.println("Quelle niveau de vie (entre 0 et 100) souhaitez-vous donner à votre guerrier ?");
                 int nomNiveauVie = sc.nextInt();
                 guerrier.setNiveauVie(nomNiveauVie); // Attribuer un niveau de vie au nouveau personnage créé
+
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 100
+                    while (nomNiveauVie < 0 || nomNiveauVie > 100) {
+
+                        System.out.println("Un niveau de vie entre 0 et 100 est indispensable"+"\nQuel niveau choisissez-vous ?");
+                        nomNiveauVie = sc.nextInt();
+                        guerrier.setNiveauVie(nomNiveauVie);
+                    }
 
                 //************************ Donner une force d'attaque au personnage ****************************************
                 System.out.println("Quelle force d'attaque souhaitez-vous donner à votre guerrier ?");
                 int nomForceAttaque = sc.nextInt();
                 guerrier.setForceAttaque(nomForceAttaque); // Attribuer une force d'attaque au nouveau personnage créé
 
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 200
+                    while (nomForceAttaque < 0 || nomForceAttaque > 200) {
+
+                        System.out.println("Une force d'attaque entre 0 et 200 est indispensable"+"\nQuel force choisissez-vous ?");
+                        nomForceAttaque = sc.nextInt();
+                        guerrier.setForceAttaque(nomForceAttaque);
+                    }
+
                 //************************ Donner un bouclier au personnage ****************************************
                 System.out.println("Quelle force souhaitez-vous donner au bouclier de votre guerrier ?");
                 int nomBouclier = sc.nextInt();
                 guerrier.setBouclier(nomBouclier); // Attribuer un bouclier au nouveau personnage créé
+
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 200
+                    while (nomBouclier < 0 || nomBouclier > 200) {
+
+                        System.out.println("Une force de bouclier entre 0 et 200 est indispensable"+"\nQuel force de bouclier choisissez-vous ?");
+                        nomBouclier = sc.nextInt();
+                        guerrier.setBouclier(nomBouclier);
+                    }
 
                 //Afficher les infos du personnage créé
                 // System.out.println(guerrier.getBouclier());// Permet de récupérer le
@@ -146,19 +170,43 @@ public class Jeu {
                     }
 
                 //************************ Donner un niveau de vie au personnage ****************************************
-                System.out.println("Quelle niveau de vie souhaitez-vous donner à votre magicien ?");
+                System.out.println("Quelle niveau de vie (entre 0 et 100) souhaitez-vous donner à votre magicien ?");
                 int niveauVieMage = sc.nextInt();
                 magicien.setNiveauVie(niveauVieMage); // Attribuer un niveau de vie au nouveau personnage créé
 
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 100
+                    while (niveauVieMage < 0 || niveauVieMage > 100) {
+
+                        System.out.println("Un niveau de vie entre 0 et 100 est indispensable."+"\nQuel niveau de vie souhaitez-vous pour votre magicien ?");
+                        niveauVieMage = sc.nextInt();
+                        magicien.setNiveauVie(niveauVieMage);
+                    }
+
                 //************************ Donner une force d'attaque au personnage ****************************************
-                System.out.println("Quelle force d'attaque souhaitez-vous donner à votre magicien ?");
+                System.out.println("Quelle force d'attaque (entre 0 et 200) souhaitez-vous donner à votre magicien ?");
                 int forceAttaqueMage = sc.nextInt();
                 magicien.setForceAttaque(forceAttaqueMage); // Attribuer une force d'attaque au nouveau personnage créé
+
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 200
+                    while (forceAttaqueMage < 0 || forceAttaqueMage > 100) {
+
+                        System.out.println("Une force d'attaque entre 0 et 200 est indispensable."+"\nQuel force d'attaque souhaitez-vous pour votre magicien ?");
+                        forceAttaqueMage = sc.nextInt();
+                        magicien.setForceAttaque(forceAttaqueMage);
+                    }
 
                 //************************ Donner un philtre au personnage ****************************************
                 System.out.println("Quelle force souhaitez-vous donner au philtre de votre magicien ?");
                 int philtreMage = sc.nextInt();
                 magicien.setPhiltre(philtreMage); // Attribuer un philtre au nouveau personnage créé
+
+                    // Condition pour limiter l'entrée à un chiffre entre 0 et 200
+                    while (philtreMage < 0 || philtreMage > 200) {
+
+                        System.out.println("Une force entre 0 et 200 est indispensable pour le philtre."+"\nQuel force souhaitez-vous pour le philtre de votre magicien ?");
+                        philtreMage = sc.nextInt();
+                        magicien.setPhiltre(philtreMage);
+                    }
 
                 //Afficher les infos du personnage créé
                 System.out.println("************************************" + "\nVotre magicien " + nomMage + " a été créé" + "\navec les caractéristiques suivantes :" + "\n ");
